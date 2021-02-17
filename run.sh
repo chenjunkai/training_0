602e330b3c86a277ae14a7f9e404d5cc744fc6dc
chenjunkai <chenjunkai.art@gmail.com>
2021-02-17 22:21:58 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -2,10 +2,10 @@
 
 # This are the hyperparameters you can change to fit your data
 python train.py --data_path=./data \
---rnn_size 128 \
+--rnn_size 512 \
 --num_layers 2 \
---seq_length 50 \
---batch_size 50 \
---num_epochs 50 \
+--seq_length 128 \
+--batch_size 128 \
+--num_epochs 128 \
 --save_checkpoints ./checkpoints \
 --save_model ./models
