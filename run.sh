fca31ad8c0360307337c5d2cc6262c8c29fbf6d1
chenjunkai <chenjunkai.art@gmail.com>
2021-02-17 22:23:16 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -5,7 +5,7 @@ python train.py --data_path=./data \
 --rnn_size 512 \
 --num_layers 2 \
 --seq_length 128 \
---batch_size 128 \
---num_epochs 128 \
+--batch_size 64 \
+--num_epochs 64 \
 --save_checkpoints ./checkpoints \
 --save_model ./models
